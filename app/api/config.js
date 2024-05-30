d0671d21dc3c86e614a908d9c44e00555aac0ada
1wemonk <boringgtoreitup@gmail.com>
2024-05-30 17:36:28 +0300

    backend fix

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pinbackewemonk.nomorepartiesco.ru/api'
+export const BASE_URL = 'https://pinbackwemonk.nomorepartiesco.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
