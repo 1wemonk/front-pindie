c186da879065d8abbb74b7df9fea727fc5ef1905
1wemonk <boringgtoreitup@gmail.com>
2024-05-30 17:04:33 +0300

    апи

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindiebacksrrnt.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://pinbackewemonk.nomorepartiesco.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
